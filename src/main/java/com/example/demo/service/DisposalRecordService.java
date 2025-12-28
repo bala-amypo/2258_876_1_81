076b9d1ec84ea54ad3e0ffd7d6e7db6287ded939
bala-amypo <bala@amypo.in>
2025-12-28 14:38:14 +0000

    User commit

--- src/main/java/com/example/demo/service/DisposalRecordService.java
+++ src/main/java/com/example/demo/service/DisposalRecordService.java
@@ -1,11 +1,15 @@
 package com.example.demo.service;
 
+import com.example.demo.dto.DisposalRequest;
 import com.example.demo.entity.DisposalRecord;
 
 import java.util.List;
 
 public interface DisposalRecordService {
-    DisposalRecord createDisposal(Long assetId, DisposalRecord disposal);
-    DisposalRecord getDisposal(Long id);
+
+    DisposalRecord createDisposal(Long assetId, DisposalRequest request);
+
     List<DisposalRecord> getAllDisposals();
+
+    DisposalRecord getDisposal(Long id);
 }
